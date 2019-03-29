b5f01f46c19e009c478d2d8ea29494bc556c4f5c
ahsanMah <mahmood.ahsan@outlook.com>
2019-03-29 16:51:46 -0400

    Started incorporating full cycle gan into model class

--- model.py
+++ model.py
@@ -4,29 +4,72 @@ import numpy as np
 
 from tensorflow.keras.layers import Input, Dense
 
-class CycleGAN:
-  def __init__(self,
-               lambda1=10,
-               lambda2=10,
-               learning_rate=2e-4,
-               beta1=0.5
-              ):
+
+# """Description 
+
+# Parameters
+# ----------
+# file_loc : str
+#     The file location of the spreadsheet
+# print_cols : bool, optional
+#     A flag used to print the columns to the console (default is
+#     False)
+
+# Returns
+# -------
+# list
+#     a list of strings used that are the header columns
+# """
+
+
+class RoboGAN:
+    
+    def __init__(self,nDimX, nDimY, \
+                lambda1=10,lambda2=10, \
+                learning_rate=2e-4,beta1=0.5):      
         """
-        Args:
-        lambda1: integer, weight for forward cycle loss (X->Y->X)
-        lambda2: integer, weight for backward cycle loss (Y->X->Y)
-        learning_rate: float, initial learning rate for Adam
-        beta1: float, momentum term of Adam
+        Parameters
+        ----------
+            lambda1: integer, weight for forward cycle loss (X->Y->X)
+            lambda2: integer, weight for backward cycle loss (Y->X->Y)
+            learning_rate: float, initial learning rate for Adam
+            beta1: float, momentum term of Adam 
         """
+        
         self.lambda1 = lambda1
         self.lambda2 = lambda2
         self.learning_rate = learning_rate
         self.beta1 = beta1
 
-        self.cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+        self.BCE = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+        
+        # Generating forward GAN = G: X -> Y
+        X = tf.placeholder(tf.float32, shape=(None,nDimX), name = "X")
+        self.G = self.generator(name="G")
+        self.D_Y = self.discriminator(name="D_Y")
 
-    def generator(self,name,hidden_nodes = [10,10], input_dim = 2, output_dim = 2,
-                    initializer = tf.keras.initializers.he_normal()):
+        # Generating backward GAN = F: Y -> X
+        Y = tf.placeholder(tf.float32, shape=(None, nDimY), name="Y")
+        self.F = self.generator(name="F")
+        self.D_X = self.discriminator(name="D_X")
+
+        return
+
+
+    # def create_model(nDimX, nDimY):
+    #     # Generating forward GAN = G: X -> Y
+    #     X = tf.placeholder(tf.float32, shape=(None,nDimX), name = "X")
+    #     self.G = self.generator(name="G")
+    #     D_Y = GAN.discriminator(name="D_Y")
+
+    #     # Generating backward GAN = F: Y -> X
+    #     Y = tf.placeholder(tf.float32, shape=(None, nDimY), name="Y")
+    #     F = GAN.generator(name="F")
+    #     D_X = GAN.discriminator(name="D_X")
+
+
+
+    def generator(self, name, hidden_nodes = [10,10], input_dim = 2, output_dim = 2, initializer = tf.keras.initializers.he_normal() ):
         
         with tf.variable_scope(name):
             # Tells model to expect batches of input_dim features
@@ -34,10 +77,9 @@ class CycleGAN:
 
             gen = tf.keras.Sequential([
                 Dense(units = hidden_nodes[0], activation="elu", input_dim=input_dim, kernel_initializer=initializer),
-                Dense(units = hidden_nodes[1], activation="elu",kernel_initializer=initializer),
-                Dense(units = output_dim, activation="tanh", kernel_initializer=initializer)
+                Dense(units = hidden_nodes[1], activation="tanh",kernel_initializer=initializer),
+                Dense(units = output_dim, kernel_initializer=initializer)
                 ])
-            # print(gen.summary())
 
         return gen
 
@@ -51,38 +93,47 @@ class CycleGAN:
             disc = tf.keras.Sequential([
                 Dense(units = hidden_nodes[0], activation="elu", input_dim=input_dim, kernel_initializer=initializer),
                 Dense(units = hidden_nodes[1], activation="elu",kernel_initializer=initializer),
-                Dense(units = 1, activation="elu", kernel_initializer=initializer)
+                Dense(units = 1,kernel_initializer=initializer)
                 ])
             # print(disc.summary())
 
         return disc
 
-    def cycle_consistency_loss(self, G, F, x, y):
-        """ cycle consistency loss (L1 norm)
+
+    # def get_optimizers():
+
+
+
+
+    def cycle_consistency_loss(self, G_Fx, F_Gy, x, y):
+        """ Cycle consistency loss (L1 norm)
         """
-        forward_loss = tf.reduce_mean(tf.abs(F(G(x))-x))
-        backward_loss = tf.reduce_mean(tf.abs(G(F(y))-y))
+        forward_loss = tf.reduce_mean(tf.abs(F_Gy-x))
+        backward_loss = tf.reduce_mean(tf.abs(G_Fx-y))
         loss = self.lambda1*forward_loss + self.lambda2*backward_loss
         return loss
 
-    def generator_loss(self, fake_output, heuristic=False):
+    
+    # NOTE: Both losses assume non-sigmoid logits as input
+
+    def generator_loss(self, fake_output, heuristic=True):
         """
         Either use Binary Cross Entropy to approximate E[ D (G(x)) ]
         or the heuristic suggested in the original Goodfellow
         """
 
         if heuristic:
-            return -0.5 * tf.reduce_mean(safe_log(fake_output))
+            return -0.5 * tf.reduce_mean(self.safe_log(fake_output))
 
-        return cross_entropy(tf.ones_like(fake_output), fake_output)
+        return self.BCE(tf.ones_like(fake_output), fake_output)
 
     def discriminator_loss(self, real_output, fake_output):
 
-        real_loss = cross_entropy(tf.ones_like(real_output), real_output)
-        fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
+        real_loss = self.BCE(tf.ones_like(real_output), real_output)
+        fake_loss = self.BCE(tf.zeros_like(fake_output), fake_output)
         total_loss = 0.5 * (real_loss + fake_loss)
 
         return total_loss
 
-    def safe_log(X, epsilon = 1e-24):
+    def safe_log(self, X, epsilon = 1e-24):
         return tf.log(X+epsilon)
